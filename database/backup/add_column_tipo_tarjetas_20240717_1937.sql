74bc358728640521ced2a1adbd3281a3be17117e
miguel-GF <miguel.gf.130@gmail.com>
2024-07-18 14:11:46 -0600

    [DB] Se actualiza sql de nuevos campos y se agrega comment para hacer cambio de esos valores

--- database/backup/add_column_tipo_tarjetas_20240717_1937.sql
+++ database/backup/add_column_tipo_tarjetas_20240717_1937.sql
@@ -1,1 +1,4 @@
 ALTER TABLE tarjetas ADD COLUMN tipo INT DEFAULT 1;
+ALTER TABLE tarjetas ADD COLUMN dia_corte VARCHAR(2);
+ALTER TABLE tarjetas ADD COLUMN dia_pago VARCHAR(2);
+-- PASAR A STRING codigo_cvv, anio_expiracion
